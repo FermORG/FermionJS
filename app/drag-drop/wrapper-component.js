00d923a829a90228c624efa035320db471fdae3e
Chris Amori <chris.amori93@gmail.com>
2017-08-07 19:55:28 -0700

    Refactor workspace and wrapper component for improved seperation of concerns

--- app/drag-drop/wrapper-component.js
+++ app/drag-drop/wrapper-component.js
@@ -3,20 +3,13 @@ import { DragSource, DropTarget } from 'react-dnd';
 import { ItemTypes } from './item-types';
 import { dragTarget, dropTarget } from './specification';
 import { dragCollect, dropCollect } from './collectors';
+import ReactResizeDetector from 'react-resize-detector';
 
 // Connect and wrap
-export default function (Component, props, children) {
-  const divWrappedComponent = (
-    <div style={{ display: 'inline-block', margin: '0', padding: '0' }}>
-      <Component {...props}>
-        { children }
-      </Component>
-    </div>
-  );
-
+export default function (Component) {
   const dndConnectedComponent = (props) => {
     const { connectDragSource, connectDropTarget } = props;
-    return connectDragSource(connectDropTarget(divWrappedComponent));
+    return connectDragSource(connectDropTarget(Component));
   };
 
   const dragWrappedComponent =
