1973bca57c1e45465f6b93583f741f51486db20d
Jeff Yamamoto <jeffmagician@gmail.com>
2017-08-13 20:37:21 -0700

    Revert unnecessary changes

--- app/components/Core.js
+++ app/components/Core.js
@@ -18,7 +18,6 @@ import Right from '../containers/RightPage.js';
 
 // Main editor component
 import Workspace from '../containers/Workspace';
-import App from '../export/App/App'
 
 // Visual component exporter function
 import getVisComponent from './VisComponents/exporter';
@@ -83,7 +82,6 @@ class Core extends Component {
             </header>
             <div className = {coreStyles.pads} data-tid='AppContainer'>
               <Workspace />
-               <App /> 
             </div>
            
             <div className = {`${this.state.hideEditor ? coreStyles.hideEditor : ""} ${coreStyles.pads}`}>
