b2dd34f8ed431e4f9fe33520c0d63ede0a7aed71
Garrett Levy <garrettlr@gmail.com>
2017-08-13 19:37:57 -0700

    adds testing for types

--- test/actions/CompListActions.spec.js
+++ test/actions/CompListActions.spec.js
@@ -1,6 +1,8 @@
 import { AddComponents, AddToProject } from '../../app/actions/complist';
+import { ADD_COMPONENTS, ADD_TO_PROJECT } from '../../app/actions/complist';
 
 describe('Component List Actions', () => {
+
   describe('ADD_COMPONENTS', () => {
     it('should return an object with the right type and an array of component objects to add to the component list', ()=>{
       expect(AddComponents([{'one': 'five'}])).toEqual({
@@ -9,6 +11,7 @@ describe('Component List Actions', () => {
       });
     });
   });
+
   describe('ADD_TO_PROJECT', () => {
     it('should return the correct action type and a component object to add to the workspace.', () => {
       expect(AddToProject({One:'Two'})).toEqual({
@@ -17,4 +20,21 @@ describe('Component List Actions', () => {
       });
     });
   });
+
+  describe('Type Testing', ()=>{
+
+    describe('ADD_COMPONENTS', ()=>{
+      it('should equal ADD_COMPONENTS', ()=>{
+        expect(ADD_COMPONENTS).toBe('ADD_COMPONENTS');
+      });
+    });
+
+    describe('ADD_TO_PROJECT', ()=>{
+      it('should equal ADD_TO_PROJECT', ()=>{
+        expect(ADD_TO_PROJECT).toBe('ADD_TO_PROJECT');
+      });
+    });
+
+  });
+
 });
