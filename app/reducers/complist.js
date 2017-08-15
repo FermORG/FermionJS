84f7f0407c1fa1a5b48f72d32e972e0bef992df5
Garrett Levy <garrettlr@gmail.com>
2017-08-14 19:54:35 -0700

    comments out json deep clone hack

--- app/reducers/complist.js
+++ app/reducers/complist.js
@@ -45,7 +45,7 @@ const defaultData = {
 
 export default function listReducer(state: {} = defaultData, action: actionType) {
   const newState = Object.assign({}, state);
-  newState.availableComponents =JSON.parse(JSON.stringify(newState.availableComponents));
+  // newState.availableComponents =JSON.parse(JSON.stringify(newState.availableComponents));
   switch (action.type) {
     case ADD_COMPONENTS:
       return newState.availableComponents.concat(action.components);
