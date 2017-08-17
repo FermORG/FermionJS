df29095768c66a5d477b5fd8d15187d13ae9b93d
Jeff Yamamoto <jeffmagician@gmail.com>
2017-08-16 21:43:45 -0700

    Remove unused component

--- app/components/Core.js
+++ app/components/Core.js
@@ -16,7 +16,7 @@ import Right from '../containers/RightPage.js';
 // Main editor component
 import Workspace from '../containers/Workspace';
 // import Preview from '../containers/Preview'
-import App from '../export/App/App'
+// import App from '../export/App/App'
 
 // Visual component exporter function
 import getVisComponent from './VisComponents/exporter';
