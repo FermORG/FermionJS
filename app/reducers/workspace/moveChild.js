372810df944480ebf93f9caf0f0e99d4ec313b37
Chris Amori <chris.amori93@gmail.com>
2017-08-18 14:30:45 -0700

    Refactory reducers/moveChild

--- app/reducers/workspace/moveChild.js
+++ app/reducers/workspace/moveChild.js
@@ -15,48 +15,11 @@ export default function moveChild(state, action) {
     return state;
   }
 
-  const [targetWidth, targetHeight] = [
-    state.components[action.targetID].props.style.width,
-    state.components[action.targetID].props.style.height
-  ];
-
-  const [sourceWidth, sourceHeight] = [
-    state.components[action.sourceID].props.style.width,
-    state.components[action.sourceID].props.style.height
-  ];
-
-  console.log(targetWidth, targetHeight, sourceWidth, sourceHeight)
-
-  let newStyle = {};
-
-  if (sourceWidth < targetWidth && sourceHeight < targetHeight){
-    newStyle = {
-      left: 0,
-      top: 0,
-      width: sourceWidth,
-      height: sourceHeight
-    }
-  }else{
-    newStyle = {
-      left: 0,
-      top: 0,
-      width: parseInt(targetWidth.split('px')[0]) * 0.5 + 'px',
-      height: parseInt(targetHeight.split('px')[0]) * 0.5 + 'px'
-    }
-
-  }
-
-  
-
-  const { components } = updateStyle(state, { type: UPDATE_STYLE, sourceID: action.sourceID, newStyle });
+  const components = { ...state.components };
   const source = cloneComponentAndChildren(components[action.sourceID]);
   const target = cloneComponentAndChildren(components[action.targetID]);
   const prevParent = cloneComponentAndChildren(components[source.parentID]);
-
-  // const [sourceWidth, sourceHeight] = [source.props.style.width, source.props.style.height];
-  // const [targetWidth, targetHeight] = [target.props.style.width, target.props.style.height];
-
- 
+  [source, target, prevParent].forEach(component => components[component.id] = component);
 
   if (source.parentID === target.id || target.parentID === source.id) {
     return state;
@@ -64,14 +27,31 @@ export default function moveChild(state, action) {
 
   source.parentID = target.id;
   target.children.push(source.id);
-
   prevParent.children = prevParent.children.filter(val => val !== source.id);
 
-  const newComponents = { ...components };
-  [source, target, prevParent].forEach(comp => newComponents[comp.id] = comp);
+  const [sourceWidth, sourceHeight, targetWidth, targetHeight] = [
+    source.props.style.width,
+    source.props.style.height,
+    target.props.style.width,
+    target.props.style.height
+  ].map(elem => parseInt(elem.split('px')[0]));
 
-  return {
-    ...state,
-    components: newComponents
+
+  const reduceSourceSize = sourceHeight > targetHeight || sourceWidth > targetWidth;
+  const newStyle = { left: 0, top: 0 };
+
+  if (reduceSourceSize) {
+    newStyle.width = `${targetWidth / 2}px`;
+    newStyle.height = `${targetHeight / 2}px`;
+  }
+
+  const updateStyleAction = {
+    type: UPDATE_STYLE,
+    sourceID: source.id,
+    newStyle
   };
+
+  const newState = updateStyle({ ...state, components }, updateStyleAction);
+
+  return newState;
 }
