99da87ea01886639967d6d36321b67ad5b815e1f
Garrett Levy <garrettlr@gmail.com>
2017-08-20 12:40:21 -0700

    builds event map, begins building flat events.

--- app/utilities/eventsRecursor.js
+++ app/utilities/eventsRecursor.js
@@ -1,13 +1,101 @@
 const { cloneDeep } = require('lodash');
+const WORKSPACE_ID = 'workspace';
 /**
 * @param {object} workspace - redux top level workspace state obj
 */
 
-export function propsParser(workspace) {
+const defaultWorkspace = {
+  componentCounter: 2,
+  activeComponent: '0',
+  components: {
+    workspace: {
+      id: WORKSPACE_ID,
+      children: [0, 1],
+      events: {},
+    },
+  },
+  state: {},
+  methods: {},
+};
+
+defaultWorkspace.components[0] = {
+  id: 0,
+  name: 'BlackBox',
+  children: [],
+  parentID: WORKSPACE_ID,
+  props: {
+    style: {
+      position: 'relative',
+      height: '100px',
+      width: '100px',
+      display: 'inline-block',
+      backgroundColor: 'black',
+      overflow: 'auto',
+    },
+  },
+  events: { onClick: ()=>{console.log('test')} }
+};
+
+defaultWorkspace.components[1] = {
+  id: 1,
+  name: 'BlueBox',
+  children: [],
+  parentID: WORKSPACE_ID,
+  props: {
+    style: {
+      position: 'relative',
+      height: '100px',
+      width: '100px',
+      display: 'inline-block',
+      backgroundColor: 'blue',
+      overflow: 'auto',
+    },
+  },
+  events: { test: 'event test =]' }
+};
+
+/*export*/ function eventsParser(workspace) {
   const clonedWorkspace = cloneDeep(workspace);
   const components = clonedWorkspace.components;
   const app = components.app || components.workspace;
-  let state = clonedWorkspace.state;
-  state = Object.assign(state, getChildProps(app, components));
+  let events = app.events;
+  events = Object.assign(events, getChildEvents(app, components));
   return clonedWorkspace;
 }
+
+function getChildEvents(parent, components) {
+  const { children } = parent;
+  let events = parent.events || {};
+  if (children.length === 0){
+    return events;
+  }
+  children.forEach((child) => {
+     events[child] = Object.assign({}, getChildEvents(components[child], components));
+  });
+
+  return events;
+}
+
+/**
+* @param {object} events - workspace events or component events
+* @param {string} component - string name of component
+* @param {object} components - workspace.components regardless of first param ID
+*/
+
+/*export*/ function flattenStateProps(events, component, components) {
+  const children = components[component].children;
+  events = cloneDeep(events);
+  return Object.keys(events).reduce((final, init) => {
+      if (children.indexOf(Number(init)) === -1) {
+        final[init] = events[init];
+      } else {
+        final = Object.assign(final, flattenStateProps(events[init], init, components));
+      }
+      delete final.style;
+      return final;
+  }, {});
+}
+
+const test = eventsParser(defaultWorkspace);
+
+console.log(test.components.workspace.events);
