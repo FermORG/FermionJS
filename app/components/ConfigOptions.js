f5316196a33e58609fc733b742e96dcc4a0f17b6
Garrett Levy <garrettlr@gmail.com>
2017-08-20 23:10:29 -0700

    adds possible color picker.

--- app/components/ConfigOptions.js
+++ app/components/ConfigOptions.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import styles from './photon.css';
 import coreStyles from './Core.css';
 import panelStyles from './Panels.css';
-
+import ColorPicker from './ColorPicker';
 // returns renderable config Panel options.
 
 export default function ConfigOption(props) {
@@ -12,11 +12,12 @@ export default function ConfigOption(props) {
     <li className={`${styles['list-group-item']}  ${panelStyles.list}`}>
       <strong>{`${propKey}`}</strong>
       <strong> : </strong>
-      <input
+      {addColorPicker(props)}
+      {/* <input
         className={`${panelStyles.editField}`}
         placeholder={`${value}`}
         onKeyPress={(event) => actionHandler(event, action, activeComponent, propKey)}
-      />
+      /> */}
       <div
         className={`${panelStyles.deleteKey}`}
         onClick={() => { onClick(deleter, activeComponent, propKey); }}
@@ -25,4 +26,20 @@ export default function ConfigOption(props) {
       </div>
     </li>
   );
+}
+
+function addColorPicker(props) {
+  const { activeComponent, propKey, value, action, deleter, actionHandler, onClick } = props;
+  console.log(propKey);
+  if (propKey.indexOf('Color') === -1) {
+    return (
+      <input
+        className={`${panelStyles.editField}`}
+        placeholder={`${value}`}
+        onKeyPress={(event) => actionHandler(event, action, activeComponent, propKey)}
+      />
+    );
+  } else {
+    return (<ColorPicker />);
+  }
 }
