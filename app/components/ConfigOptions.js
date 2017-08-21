0cd1b7e5da8172daeadbd15847de474b59673428
Henry (Hai) Zhu <henryhaizhu@gmail.com>
2017-08-21 16:36:47 -0700

    added some css

--- app/components/ConfigOptions.js
+++ app/components/ConfigOptions.js
@@ -6,22 +6,39 @@ import panelStyles from './Panels.scss';
 
 // returns renderable config Panel options.
 
-export default function ConfigOption(props) {
-  const { activeComponent, propKey, value, action, deleter, actionHandler, onClick } = props;
-  return (
-    <li className={`${styles['list-group-item']}  ${panelStyles.list}`}>
-      <strong>{`${propKey}`}</strong>
-      <strong> : </strong>
-      <input
-        className={`${panelStyles.editField}`}
-        onKeyPress={(event) => actionHandler(event, action, activeComponent, propKey)}
-        defaultValue={value}
-      />
+export default class ConfigOption extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      deleteButton: ""
+    }
+  }
+  render(){
+    const { activeComponent, propKey, value, action, deleter, actionHandler, onClick } = this.props;
+    return (
+      <li className={`${styles['list-group-item']}  ${panelStyles.list}`}>
+        <strong>{`${propKey}`}</strong>
+        <strong> : </strong>
+        <input
+          className={`${panelStyles.editField}`}
+          onKeyPress={(event) => actionHandler(event, action, activeComponent, propKey)}
+          defaultValue={value}
+        />
+        <a className={`${coreStyles['deleteKey']}`}
+          onClick={() => { onClick(deleter, activeComponent, propKey)}}
+          onMouseOver={() => {this.setState({deleteButton: "X"})}}
+          onMouseLeave={() => {this.setState({deleteButton: ""})}}>
+          {this.state.deleteButton}
+        </a> 
+    
+      </li>
+    );
+  }
 
-       <a className={`${coreStyles['btn']} ${coreStyles['btn-blue']} ${styles['pull-right']}`}
-        onClick={() => { onClick(deleter, activeComponent, propKey)}}>
-         X
-      </a> 
-    </li>
-  );
 }
+ 
+
+// <a className={`${coreStyles['btn']} ${coreStyles['btn-blue']} ${styles['pull-right']}`}
+//   onClick={() => { onClick(deleter, activeComponent, propKey)}}>
+//   X
+// </a> 
