0bcb2069951ea6af3ce05fd9ee3ed0bcabbd1896
Garrett Levy <garrettlr@gmail.com>
2017-08-23 09:31:20 -0700

    updates reducer to remove textEditor Note on type

--- app/reducers/workspace/updateMethods.js
+++ app/reducers/workspace/updateMethods.js
@@ -1,20 +1,22 @@
 export default function updateMethods(state, methods) {
   const nextState = Object.assign({}, state);
-  nextState.methods = methods;
+  nextState.methods = removeNote(methods);
   nextState.methodNames = methodNames(methods);
   return nextState;
 }
 
 
+function removeNote(methods) {
+  return methods.split('/*Anything you type in here will be appended to App.js as a \n method. you can then attach them as event handlers, logic handlers, etc.*/').join('');
+}
+
 function methodNames(methods) {
   const methodArray = methods.split('}@\n');
   const methodNames = methodArray.map((method) => {
-    // const end = method.indexOf('()');
     let end = method.match(/\((.+)\)/);
     if (end) return method.slice(0, end.index).trim();
     else end = method.indexOf('()');
     return method.slice(0, end).trim();
-    //return method.replace(/\((.+)\)/, '');
   });
   if (methodNames[methodNames.length - 1] === '') methodNames.pop();
   return methodNames;
