b25af10747bcade42218c619578211d273318111
Chris Amori <chris.amori93@gmail.com>
2017-08-23 21:56:17 -0700

    update exporter to iterate through current directory, fetching all jsx exports from .jsx file types

--- app/components/VisComponents/exporter.js
+++ app/components/VisComponents/exporter.js
@@ -1,9 +1,17 @@
-import BlackBox from './BlackBox';
-import BlueBox from './BlueBox';
-import Square from './Square';
-import Row from './Square';
-import Board from './Square';
-const components = { BlackBox, BlueBox, Square, Row, Board };
+const fs = require('fs');
+const path = require('path');
+
+const components = fs
+  .readdirSync(path.join(__dirname, '/components/VisComponents'))
+  .filter(file => path.extname(file) === '.jsx')
+  .reduce((acc, file) => {
+    const fileName = file.split('.jsx')[0];
+    const { jsx } = require(`./${file}`);
+    acc[fileName] = jsx;
+    return acc;
+  }, {});
+
+console.log(components);
 
 const getVisComponent = (key) => components[key];
 
