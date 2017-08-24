69e8625fbab2b7af390f3e21f8beeb597b48c977
Chris Amori <chris.amori93@gmail.com>
2017-08-23 21:57:28 -0700

    Create componentLibrary.js, which iterates through all components within library and exporting necessary data to add to state

---
+++ app/components/VisComponents/componentLibrary.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const path = require('path');
+
+const overrideRequiredComponentStyle = {
+  position: 'absolute'
+};
+
+const defaultRequiredComponentStyle = {
+  width: '100px',
+  height: '100px'
+};
+
+const getComponentLibrary = (directory = __dirname) => {
+  const componentList = fs
+    .readdirSync(directory)
+    .filter(file => path.extname(file) === '.jsx')
+
+  return componentList.map(file => {
+    const { style } = require(path.join(directory, file));
+    const componentData = { name: path.name(file) };
+
+    {
+      ...defaultRequiredComponentStyles,
+      ...style,
+      ...overrideRequiredComponentStyle
+    };
+
+    return componentData;
+  });
+}
