eab9dc2cc0c4156177b15d1cea4a2bbee3cd85b0
Garrett Levy <garrettlr@gmail.com>
2017-08-26 18:20:38 -0700

    fully implements import components.

--- app/ImportComponent.js
+++ app/ImportComponent.js
@@ -1,18 +1,17 @@
 // allows users to import Custom Components
 import electron from 'electron';
-// import remote from 'remote';
 const { dialog } = electron;
 import fs from 'fs';
+import path from 'path';
 
+const libraryPath = path.join(__dirname + '/component-library/');
 const options = {
   title: 'Select Components',
   buttonLabel: 'Import',
   filters: [
     {
       name: 'React Components',
-      extensions: [
-      'jsx',
-     ],
+      extensions: ['jsx'],
     },
   ],
   properties: [
@@ -26,15 +25,14 @@ const importWrapper = () => {
     if (!fileNames) {
       return;
     }
-    console.log(fileNames);
 
-    // fs.readFile(filepath, 'utf-8', (err, data) => {
-    //   if (err) {
-    //     console.log('an error occured!');
-    //     return;
-    //   }
-    //   console.log(data);
-    // });
+    fileNames.forEach((file) => {
+      const name = file.lastIndexOf('/');
+      const fileName = file.slice(name + 1);
+      const savePath = path.join(libraryPath + fileName);
+      fs.createReadStream(file).pipe(fs.createWriteStream(savePath));
+    });
+
   });
 }
 
